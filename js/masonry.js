fdda1baad5f1b7c704a79589c822e077a86e6bbf
Diggen85 <benny_stark@live.de>
2019-09-21 23:51:09 +0200

    Step to v0.2.0
    # v0.2.0
    ##  09/21/2019
    
    1. [](#new)
        * Masonry Blog option
    		* Added Item / Card Type option to change the cells count (width)
    		* Added Item / Card Tpye - 6 Cell Post Summary with Title above Image
    2. [](#improved)
    		* Changed naming of Blog Layouts
    		* Changed About Page
    3. [](#bugfix)
    		* removed extra ' in shortcode
    		* removed none MDL Images

---
+++ js/masonry.js
@@ -0,0 +1,14 @@
+
+var msnry = new Masonry( '.masonry-grid' , {
+  // options
+  //itemSelector: '.masonry-item',
+	itemSelector: '.mdl-card',
+	columnWidth: 75,
+	// fitWidth: true,
+	// percentPosition: true,
+	// horizontalOrder: true
+	// originLeft: false,
+	// originTop: false,
+	transitionDuration: '0.8s',
+	stagger: 30,
+});
